91377caa5865461eb94454b9555075ee5efa8c23
thaneshvenkatkovuru <thaneshvenkat1234@gmail.com>
2026-01-09 20:19:32 +0530

    Add files via upload

---
+++ ChatWebSocketHandler.java
@@ -0,0 +1,34 @@
+package com.chat;
+
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+    private static final Set<WebSocketSession> sessions =
+            new CopyOnWriteArraySet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message)
+            throws Exception {
+
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen()) {
+                s.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+    }
+}
